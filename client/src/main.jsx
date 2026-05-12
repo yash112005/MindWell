d6ec5e749806f32366fe096046c82c99b59856ab
yash112005 <ynamdeo248@gmail.com>
2026-05-12 13:13:48 +0530

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,10 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import './i18n'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
